4bb15dfd44ee169088b5b0abcbfd3eaff583a414
egimoro <gimoremma@gmail.com>
2020-08-26 14:59:09 +0100

    Added string to model.py

--- crud_ajax/models.py
+++ crud_ajax/models.py
@@ -4,3 +4,6 @@ class CrudUser(models.Model):
     name = models.CharField(max_length=30, blank=True)
     address = models.CharField(max_length=100, blank=True)
     age = models.IntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name 
